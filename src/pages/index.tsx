392656d968d783019d03394d17e6b904ec11245b
Fábio Alves <fabio.alves@chuva.io>
2022-06-03 16:01:56 -0100

    Initial commit from Create Next App

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | FabyoSK</title>
+      </Head>
+
+      <main>
+        <h1>Best blog everrrrrrr - FSKZinho</h1>
+      </main>
+    </>
+  )
+}
---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Post() {
+  return (
+    <>
+      <Head>
+        <title>Blog | FabyoSK Blog</title>
+      </Head>
+
+      <main>
+        <p>Post example</p>
+      </main>
+    </>
+  )
+}
