c833e5a573277e977e7a597e01f0b26e0945c043
Fábio Alves <fabio.alves@chuva.io>
2022-06-07 15:07:14 -0100

    feat: add analitys

---
+++ src/pages/_document.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500&display=swap"
+            rel="stylesheet"
+          />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-FT0JMV6RH4" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-FT0JMV6RH4');
+            `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
